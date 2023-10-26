69aa102d0276ed141f97718ed5fd83bd42bd8386
aaout <aaout8@gmail.com>
2023-10-27 01:26:09 +0900

    first commit

---
+++ scripts/calc_voxel_histgram.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+from tqdm import tqdm
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import torch
+from PIL import Image
+import nibabel
+
+if __name__ == "__main__":
+    NUMBER = "006"
+    CHANNEL = "flair"
+    # CHANNEL = "t1"
+    # CHANNEL = "t1ce"
+    # CHANNEL = "t2"
+    voxel_path = f"/mnt/ito/diffusion-anomaly/data/archive/BraTS2020_TrainingData/BraTS20_Training_{NUMBER}/BraTS20_Training_{NUMBER}_{CHANNEL}.nii"
+    nib_voxel = nibabel.load(voxel_path)
+    np_voxel = nib_voxel.get_fdata()
+
+    # 各スライスの背景を除くピクセル値の合計を格納するリスト
+    total_pixel_values = []
+
+    for slice_number in range(np_voxel.shape[2]):
+        slice_data = np_voxel[:, :, slice_number]
+        pixel_values = slice_data.ravel()
+        non_zero_pixel_values = pixel_values[pixel_values != 0]
+        total_pixel_values.extend(non_zero_pixel_values)  # ヒストグラム用の変数に追加
+
+    # ピクセル値の最大、最小、クリップ点が上位何%にあるかを表示
+    # clip_upper = 378
+    # clip_lower = 60
+    # 上位1%と下位1%の位置を計算
+    sorted_lists = sorted(total_pixel_values)
+    total_count = len(sorted_lists)
+    top_1_percent_index = int(0.99 * total_count)
+    bottom_1_percent_index = int(0.01 * total_count)
+    # 上位1%の数値を取得
+    top_1_percent_values = sorted_lists[top_1_percent_index]
+    # 下位1%の数値を取得
+    bottom_1_percent_values = sorted_lists[bottom_1_percent_index]
+
+
+    # 結果を表示
+    print(f"上位1%の数値とインデックス: {top_1_percent_values}")
+
+    print(f"下位1%の数値とインデックス: {bottom_1_percent_values}")
+    # upper_position = sorted_lists.index(clip_upper) + 1
+    # lower_position = sorted_lists.index(clip_lower) + 1
+    # upper_percentile = (upper_position / total_count) * 100
+    # lower_percentile = (lower_position / total_count) * 100
+    # print(f"{NUMBER}_{CHANNEL}")
+    # print("sum of pixels: ", total_count)
+    # print(f"max: {sorted_lists[-1]}")
+    # print(f"min: {sorted_lists[0]}")
+    # print(f"upper_percentile: {clip_upper} {upper_percentile:.4f}%")
+    # print(f"lower_percentile: {clip_lower} {lower_percentile:.4f}%")
+
+    # ピクセル値のヒストグラムを表示
+    # plt.figure(figsize=(8, 6))
+    # plt.hist(total_pixel_values, bins=100, color='b', alpha=0.7, rwidth=0.8)
+    # plt.grid(axis='y', linestyle='--', alpha=0.7)
+    # plt.tight_layout()
+    # plt.show()
+    # os.makedirs("/mnt/ito/diffusion-anomaly/out/hist/", exist_ok=True)
+    # plt.savefig(f"/mnt/ito/diffusion-anomaly/out/hist/BraTS20_Training_{NUMBER}_{CHANNEL}.jpg")
---
+++ scripts/create_dataset.py
@@ -0,0 +1,145 @@
+import os
+import sys
+from tqdm import tqdm
+import shutil
+import nibabel
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn
+from PIL import Image
+
+
+def norm_func(voxel_data):
+    """各被験者の各チャネルのボクセルデータに対し、上位1%を1, 下位1%を0, その他を線形に正規化する関数
+
+    Args:
+        voxel_data (numpy.ndarray): 各被験者の各チャネルのボクセルデータ[240, 240, 155]
+
+    Returns:
+        normalized_data (numpy.ndarray): 0-1に正規化した後のボクセルデータ[240, 240, 155]
+    """
+    total_pixel_values = []  # 各スライスの背景を除くピクセル値の合計を格納するリスト
+    # 上位1%と下位1%の値を取得
+    for slice_number in range(voxel_data.shape[2]):
+        np_slice = voxel_data[:, :, slice_number]
+        pixel_values = np_slice.ravel()
+        non_zero_pixel_values = pixel_values[pixel_values != 0]  # 背景を削除
+        total_pixel_values.extend(non_zero_pixel_values)  # ヒストグラム用の変数に追加
+    sorted_lists = sorted(total_pixel_values)
+    total_count = len(sorted_lists)
+    max_value = sorted_lists[-1]
+    min_value = sorted_lists[0]
+    top_1_percent_index = int(0.99 * total_count)
+    bottom_1_percent_index = int(0.01 * total_count)
+    upper_clip = sorted_lists[top_1_percent_index]
+    lower_clip = sorted_lists[bottom_1_percent_index]
+    # print(f"upper clip: {upper_clip}")
+    # print(f"lower clip: {lower_clip}")
+
+    # upper_clip以上を1, lower_clip以下を0, その他は最大値をupper_clip, 最小値をlower_clipとして線形に正規化
+    normalized_data = np.clip(voxel_data, lower_clip, upper_clip)
+    normalized_data = (normalized_data - lower_clip) / (upper_clip - lower_clip)
+    # print("max: ", np.max(normalized_data))
+    # print("min: ", np.min(normalized_data))
+
+    return normalized_data
+
+
+def norm_seg_func(seg_slice_data):
+    max_value = np.max(seg_slice_data)
+    min_value = np.min(seg_slice_data)
+    if max_value == 0:
+        normalized_seg_slice_data = seg_slice_data
+    else:
+        normalized_seg_slice_data = (seg_slice_data - min_value) / (max_value - min_value)
+    return normalized_seg_slice_data
+
+
+if __name__ == "__main__":
+    FLAG = "train"  # train or test
+    if FLAG == "train":
+        DATA_DIR = "/mnt/ito/diffusion-anomaly/data/archive/BraTS2020_TrainingData"
+    elif FLAG == "test":
+        DATA_DIR = "/mnt/ito/diffusion-anomaly/data/archive/BraTS2020_ValidationData"
+    else:
+        print("FLAG must be train or test")
+        sys.exit()
+
+    with tqdm(os.walk(DATA_DIR)) as pbar:
+        for root, dirs, files in pbar:
+            # print(root)
+            # print(dirs)
+            # print(files)
+            # number = dirs[0].split("_")[-1]
+            # print(number)
+            if not dirs:
+                # 各被験者の各チャネル[seg, flair, t1, t1ce, t2]をfilesとして読み込む
+                if not root.endswith("BraTS20_Training_333"):
+                    continue
+                # sys.exit()
+                files = sorted(files)
+                for f in files:
+                    # seg.niiはnorm_funcを適用せず正規化
+                    if f.endswith("seg.nii"):
+                        data_path = os.path.join(root, f)
+                        nib_voxel = nibabel.load(data_path)
+                        np_voxel = nib_voxel.get_fdata()
+                        for slice_i in range(np_voxel.shape[2]):
+                            # 首側の80枚と頭頂側の26枚は使用しない
+                            if slice_i < 80 or slice_i >= 129:
+                                continue
+                            np_slice = np_voxel[:, :, slice_i]
+                            np_norm_slice = norm_seg_func(np_slice)
+                            # スライスデータの左右反転と90度回転
+                            flipped_image_data = np.fliplr(np_norm_slice)
+                            rotated_image_data = np.rot90(flipped_image_data, k=1)
+                            # testデータのlabelsだけ別のディレクトリに保存
+                            if FLAG == "test":
+                                os.makedirs(
+                                    f"/media/user/ボリューム2/brats_imgs/test_labels/{f.split('.')[0][0:20]}/slice_{str(slice_i).zfill(3)}/",
+                                    exist_ok=True,
+                                )
+                                nib_slice = nibabel.Nifti1Image(rotated_image_data, affine=np.eye(4))
+                                nibabel.save(
+                                    nib_slice,
+                                    f"/media/user/ボリューム2/brats_imgs/test_labels/{f.split('.')[0][0:20]}/slice_{str(slice_i).zfill(3)}/{f.split('.')[0][0:20]}_{f.split('.')[0].split('_')[-1]}_{str(slice_i).zfill(3)}.nii.gz",
+                                )
+                            else:
+                                os.makedirs(
+                                    f"/media/user/ボリューム2/brats_imgs/{FLAG}/{f.split('.')[0][0:20]}/slice_{str(slice_i).zfill(3)}/",
+                                    exist_ok=True,
+                                )
+                                nib_slice = nibabel.Nifti1Image(rotated_image_data, affine=np.eye(4))
+                                nibabel.save(
+                                    nib_slice,
+                                    f"/media/user/ボリューム2/brats_imgs/{FLAG}/{f.split('.')[0][0:20]}/slice_{str(slice_i).zfill(3)}/{f.split('.')[0][0:20]}_{f.split('.')[0].split('_')[-1]}_{str(slice_i).zfill(3)}.nii.gz",
+                                )
+                    else:
+                        # seg.nii以外はnorm_funcを適用して正規化
+                        data_path = os.path.join(root, f)
+                        nib_voxel = nibabel.load(data_path)
+                        np_voxel = nib_voxel.get_fdata()
+                        np_norm_voxel = norm_func(np_voxel)
+
+                        with tqdm(range(np_norm_voxel.shape[2]), leave=False) as pbar2:
+                            pbar2.set_description(f"{f.split('.')[0][0:20]}")
+                            print("\n")
+                            for slice_i in pbar2:
+                                # 首側の80枚と頭頂側の26枚は使用しない
+                                if slice_i < 80 or slice_i >= 129:
+                                    continue
+                                np_norm_slice = np_norm_voxel[:, :, slice_i]
+                                # スライスデータの左右反転と90度回転
+                                flipped_image_data = np.fliplr(np_norm_slice)
+                                rotated_image_data = np.rot90(flipped_image_data, k=1)
+
+                                os.makedirs(
+                                    f"/media/user/ボリューム2/brats_imgs/{FLAG}/{f.split('.')[0][0:20]}/slice_{str(slice_i).zfill(3)}/",
+                                    exist_ok=True,
+                                )
+                                nib_slice = nibabel.Nifti1Image(rotated_image_data, affine=np.eye(4))
+                                nibabel.save(
+                                    nib_slice,
+                                    f"/media/user/ボリューム2/brats_imgs/{FLAG}/{f.split('.')[0][0:20]}/slice_{str(slice_i).zfill(3)}/{f.split('.')[0][0:20]}_{f.split('.')[0].split('_')[-1]}_{str(slice_i).zfill(3)}_w.nii.gz",
+                                )
