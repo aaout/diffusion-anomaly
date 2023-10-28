cccad66f11151e1f9e340696a8dfecd36f58fb2d
aaout <aaout8@gmail.com>
2023-10-28 23:20:06 +0900

    formatting by black

--- guided_diffusion/bratsloader.py
+++ guided_diffusion/bratsloader.py
@@ -6,25 +6,26 @@ import os.path
 import nibabel
 from scipy import ndimage
 
+
 class BRATSDataset(torch.utils.data.Dataset):
     def __init__(self, directory, test_flag=False):
-        '''
+        """
         directory is expected to contain some folder structure:
-                  if some subfolder contains only files, all of these
-                  files are assumed to have a name like
-                  brats_train_001_XXX_123_w.nii.gz
-                  where XXX is one of t1, t1ce, t2, flair, seg
-                  we assume these five files belong to the same image
-                  seg is supposed to contain the segmentation
-        '''
+                if some subfolder contains only files, all of these
+                files are assumed to have a name like
+                brats_train_001_XXX_123_w.nii.gz
+                where XXX is one of t1, t1ce, t2, flair, seg
+                we assume these five files belong to the same image
+                seg is supposed to contain the segmentation
+        """
         super().__init__()
         self.directory = os.path.expanduser(directory)
 
-        self.test_flag=test_flag
+        self.test_flag = test_flag
         if test_flag:
-            self.seqtypes = ['t1', 't1ce', 't2', 'flair']
+            self.seqtypes = ["t1", "t1ce", "t2", "flair"]
         else:
-            self.seqtypes = ['t1', 't1ce', 't2', 'flair', 'seg']
+            self.seqtypes = ["t1", "t1ce", "t2", "flair", "seg"]
 
         self.seqtypes_set = set(self.seqtypes)
         self.database = []
@@ -35,28 +36,27 @@ class BRATSDataset(torch.utils.data.Dataset):
                 datapoint = dict()
                 # extract all files as channels
                 for f in files:
-                    seqtype = f.split('_')[3]
+                    seqtype = f.split("_")[3]
                     datapoint[seqtype] = os.path.join(root, f)
-                assert set(datapoint.keys()) == self.seqtypes_set, \
-                    f'datapoint is incomplete, keys are {datapoint.keys()}'
+                assert (
+                    set(datapoint.keys()) == self.seqtypes_set
+                ), f"datapoint is incomplete, keys are {datapoint.keys()}"
                 self.database.append(datapoint)
 
     def __getitem__(self, x):
         out = []
         filedict = self.database[x]
         for seqtype in self.seqtypes:
-            number=filedict['t1'].split('/')[4]
+            number = filedict["t1"].split("/")[4]
             nib_img = nibabel.load(filedict[seqtype])
             out.append(torch.tensor(nib_img.get_fdata()))
         out = torch.stack(out)
         out_dict = {}
         if self.test_flag:
-            path2 = './data/brats/test_labels/' + str(
-                number) + '-label.nii.gz'
+            path2 = "./data/brats/test_labels/" + str(number) + "-label.nii.gz"
 
-
-            seg=nibabel.load(path2)
-            seg=seg.get_fdata()
+            seg = nibabel.load(path2)
+            seg = seg.get_fdata()
             image = torch.zeros(4, 256, 256)
             image[:, 8:-8, 8:-8] = out
             label = seg[None, ...]
@@ -64,18 +64,18 @@ class BRATSDataset(torch.utils.data.Dataset):
                 weak_label = 1
             else:
                 weak_label = 0
-            out_dict["y"]=weak_label
+            out_dict["y"] = weak_label
         else:
-            image = torch.zeros(4,256,256)
-            image[:,8:-8,8:-8]=out[:-1,...]		#pad to a size of (256,256)
+            image = torch.zeros(4, 256, 256)
+            image[:, 8:-8, 8:-8] = out[:-1, ...]  # pad to a size of (256,256)
             label = out[-1, ...][None, ...]
-            if label.max()>0:
-                weak_label=1
+            if label.max() > 0:
+                weak_label = 1
             else:
-                weak_label=0
+                weak_label = 0
             out_dict["y"] = weak_label
 
-        return (image, out_dict, weak_label, label, number )
+        return (image, out_dict, weak_label, label, number)
 
     def __len__(self):
         return len(self.database)
