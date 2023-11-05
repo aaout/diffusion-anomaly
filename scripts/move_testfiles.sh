51e858a0ebbca32ff4a516b3b3bc115896778e72
aaout <aaout8@gmail.com>
2023-11-05 11:08:29 +0900

    validに使うデータを変更

--- scripts/move_testfiles.sh
+++ scripts/move_testfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {333..369}; do
+for i in {334..369}; do
     if [ -e "/mnt/ito/diffusion-anomaly/data/archive/BraTS2020_TrainingData/BraTS20_Training_${i}" ]; then
         sudo mv "/mnt/ito/diffusion-anomaly/data/archive/BraTS2020_TrainingData/BraTS20_Training_${i}" "/mnt/ito/diffusion-anomaly/data/archive/BraTS2020_ValidationData/BraTS20_Validation_${i}"
     fi
