febe3aec51016f04abb9a03ae624bf322c7c3a76
aaout <aaout8@gmail.com>
2023-11-07 15:52:51 +0900

    dice, aurocを記録したjsonから最大最小値を抽出

---
+++ scripts/extract_minmax_dice.py
@@ -0,0 +1,21 @@
+import json
+
+with open("/mnt/ito/diffusion-anomaly/out/dice_and_auroc.json", "r") as file:
+    data = json.load(file)
+
+min_dice_value = float('inf')
+max_dice_value = float('-inf')
+min_dice_element = None
+max_dice_element = None
+
+for key, value in data.items():
+    dice_value = value['dice']
+    if dice_value < min_dice_value:
+        min_dice_value = dice_value
+        min_dice_element = {key: value}
+    if dice_value > max_dice_value:
+        max_dice_value = dice_value
+        max_dice_element = {key: value}
+
+print(f"min dice element: {min_dice_element}")
+print(f"max dice element: {max_dice_element}")
