428b2fbea890a3afe00ad656ed904642106347b1
aaout <aaout8@gmail.com>
2023-11-07 11:34:42 +0900

    calc dice and auroc

--- scripts/calc_dice_and_auroc.py
+++ scripts/calc_dice_and_auroc.py
@@ -18,7 +18,9 @@ if __name__ == '__main__':
     dice_list = []
     auroc_list = []
     filename_list = []
+    thresh_value_list = []
     diffmap_dirs = "/media/user/ボリューム2/out/diffmap_pt/"
+
     for root, dirs, files in os.walk(diffmap_dirs):
         if not dirs:
             # ファイル名を取得
@@ -36,11 +38,34 @@ if __name__ == '__main__':
             # 入出力差分のテンソルデータを読み込み, NumPy配列に変換
             tensor_data = torch.load(diff_all_file_path)
             numpy_data = tensor_data.cpu().numpy()
+
+            # ptデータのピクセル値のヒストグラムを表示
+            plt.figure(figsize=(8, 6))
+            plt.hist(numpy_data.ravel() , bins=100, color='b', alpha=0.7, rwidth=0.8)
+            plt.grid(axis='y', linestyle='--', alpha=0.7)
+            plt.tight_layout()
+            plt.ylim(0, 2000)
+            plt.show()
+            os.makedirs(
+                f"{diffmap_dirs}/{subject_path[-1]}/{slice_id}",
+                exist_ok=True,
+            )
+            plt.savefig(f"{diffmap_dirs}/{subject_path[-1]}/{slice_id}/diffmap_hist_pt.jpg")
+            plt.close()
+
+            # pt wo normalize
             norm_image = cv2.normalize(numpy_data, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
             norm_image = norm_image.astype(np.uint8)
 
             # 入出力差分をOtsu手法により二値化
-            _, binarized_image = cv2.threshold(norm_image, 0, 255, cv2.THRESH_OTSU)
+            thresh_value, binarized_input_image = cv2.threshold(norm_image, 0, 255, cv2.THRESH_OTSU)
+            thresh_value_list.append(thresh_value)
+
+            # 二値化された入力画像を保存
+            plt.imshow(binarized_input_image, cmap='gray')
+            plt.axis('off')
+            plt.savefig(f'{diffmap_dirs}/{subject_path[-1]}/{slice_id}/binarized_diffmap_image.jpg', bbox_inches='tight', pad_inches=0)
+            plt.close()            
 
             # DICEやAUROCを計算するため, ラベルデータを読み込む
             ground_truth_mask = cv2.imread(ground_truth_mask_path, cv2.IMREAD_GRAYSCALE)
@@ -48,18 +73,25 @@ if __name__ == '__main__':
                 print(f'ground_truth_mask is None: {ground_truth_mask_path}')
                 sys.exit()
             else:
-                ground_truth_mask = (ground_truth_mask > 30).astype(np.uint8)
+                ground_truth_mask = cv2.normalize(ground_truth_mask, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+                _, binarized_ground_truth_mask = cv2.threshold(ground_truth_mask, 50, 255, cv2.THRESH_BINARY)
+                binarized_ground_truth_mask = binarized_ground_truth_mask.astype(np.uint8)
                 padded_mask = np.zeros((256, 256), dtype=np.uint8)
-                padded_mask[8:-8, 8:-8] = ground_truth_mask
+                padded_mask[8:-8, 8:-8] = binarized_ground_truth_mask
+
+                plt.imshow(padded_mask, cmap='gray')
+                plt.axis('off')
+                plt.savefig(f'{diffmap_dirs}/{subject_path[-1]}/{slice_id}/binarized_label_image.jpg', bbox_inches='tight', pad_inches=0)
+                plt.close()
 
                 # 真実のマスクと二値化された画像のDICE係数を計算
-                dice = dice_coefficient(padded_mask, binarized_image)
+                dice = dice_coefficient(padded_mask, binarized_input_image)
                 dice_list.append(dice)
                 print(f"DICE: {dice}")
 
                 # AUROCの計算
                 flat_truth = padded_mask.flatten()
-                flat_pred = binarized_image.flatten()
+                flat_pred = binarized_input_image.flatten()
                 auroc = roc_auc_score(flat_truth, flat_pred)
                 auroc_list.append(auroc)
                 print(f"AUROC: {auroc}")
@@ -72,8 +104,8 @@ if __name__ == '__main__':
     print(f"DICE average: {dice_avg}")
     print(f"AUROC average: {auroc_avg}")
     dice_and_auroc_json["average"] = {"dice": dice_avg, "auroc": auroc_avg}
-    for fname, dice, auroc in zip(filename_list, dice_list, auroc_list):
-        dice_and_auroc_json[fname] = {"dice": dice, "auroc": auroc}
+    for fname, dice, auroc, thresh in zip(filename_list, dice_list, auroc_list, thresh_value_list):
+        dice_and_auroc_json[fname] = {"dice": dice, "auroc": auroc, "threshold": thresh}
 
-    with open("/mnt/ito/diffusion-anomaly/out/dice_and_auroc_json", "w") as f:
+    with open("/mnt/ito/diffusion-anomaly/out/dice_and_auroc.json", "w") as f:
         json.dump(dice_and_auroc_json, f, indent=4)
