9966f43480ffd11b39d1d07df06d6b9899632117
aaout <aaout8@gmail.com>
2023-11-14 03:26:43 +0900

    change folder search

--- scripts/calc_dice_and_auroc.py
+++ scripts/calc_dice_and_auroc.py
@@ -19,111 +19,102 @@ if __name__ == "__main__":
     auroc_list = []
     filename_list = []
     thresh_value_list = []
-    diffmap_dirs = "/media/user/ボリューム2/out/diffmap_pt/"
+    diffmap_dirs = "/media/user/ボリューム/out/sample_data_and_heatmap/"
 
-    for root, dirs, files in os.walk(diffmap_dirs):
-        if not dirs:
-            # ファイル名を取得
-            diff_all_file = [f for f in files if f.endswith("all.pt")]
-            diff_all_file_path = os.path.join(root, diff_all_file[0])
-            slice_id = diff_all_file_path.split("/")[7]
-            subject_path = diff_all_file_path.split("/")[:7]
-            subject_path_concat = "/".join(subject_path)
-            ground_truth_mask_path = subject_path_concat + "/" + slice_id + "_label.jpg"
-            ground_truth_mask_path = ground_truth_mask_path.replace(
-                "diffmap_pt", "abnormal_sample_and_heatmap"
+    subject_dirs = sorted(os.listdir(diffmap_dirs))
+    for subject_id in subject_dirs:
+        subject_path = os.path.join(diffmap_dirs, subject_id)
+        slice_list = sorted(os.listdir(subject_path))
+        for slice_id in slice_list:
+            slice_path = os.path.join(subject_path, slice_id)
+            target_file_list = sorted(os.listdir(slice_path))
+            # 対象の被験者名とスライス名を取得
+            target_id = f"{subject_id}_{slice_id}"
+            print(f"ID: {target_id}")
+
+            # 4チャネル差分合計のファイル名を取得してnumpy形式に変換
+            diff_all_pt = [f for f in target_file_list if f.endswith("diff_all.pt")]
+            diff_all_pt_path = os.path.join(slice_path, diff_all_pt[0])
+            diff_all_pt_data = torch.load(diff_all_pt_path)
+            diff_all_np_data = diff_all_pt_data.cpu().numpy()
+            norm_diff_all_np = cv2.normalize(
+                diff_all_np_data,
+                None,
+                alpha=0,
+                beta=255,
+                norm_type=cv2.NORM_MINMAX,
             )
-            subject_id = subject_path[-1] + "_" + slice_id
-            filename_list.append(subject_id)
-            print(f"ID: {subject_id}")
-
-            # 入出力差分のテンソルデータを読み込み, NumPy配列に変換
-            tensor_data = torch.load(diff_all_file_path)
-            numpy_data = tensor_data.cpu().numpy()
-
-            # ptデータのピクセル値のヒストグラムを表示
-            plt.figure(figsize=(8, 6))
-            plt.hist(numpy_data.ravel(), bins=100, color="b", alpha=0.7, rwidth=0.8)
-            plt.grid(axis="y", linestyle="--", alpha=0.7)
-            plt.tight_layout()
-            plt.ylim(0, 2000)
-            plt.show()
-            os.makedirs(
-                f"{diffmap_dirs}/{subject_path[-1]}/{slice_id}",
-                exist_ok=True,
-            )
-            plt.savefig(
-                f"{diffmap_dirs}/{subject_path[-1]}/{slice_id}/diffmap_hist_pt.jpg"
-            )
-            plt.close()
-
-            # pt wo normalize
-            norm_image = cv2.normalize(
-                numpy_data, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX
-            )
-            norm_image = norm_image.astype(np.uint8)
-
+            norm_diff_all_np = norm_diff_all_np.astype(np.uint8)
             # 入出力差分をOtsu手法により二値化
-            thresh_value, binarized_input_image = cv2.threshold(
-                norm_image, 0, 255, cv2.THRESH_OTSU
+            thresh_value, bin_norm_diff_all = cv2.threshold(
+                norm_diff_all_np, 0, 255, cv2.THRESH_OTSU
             )
             thresh_value_list.append(thresh_value)
 
-            # 二値化された入力画像を保存
-            plt.imshow(binarized_input_image, cmap="gray")
-            plt.axis("off")
-            plt.savefig(
-                f"{diffmap_dirs}/{subject_path[-1]}/{slice_id}/binarized_diffmap_image.jpg",
-                bbox_inches="tight",
-                pad_inches=0,
+            # ラベルデータのファイル名を取得してnumpy形式に変換
+            label_pt = [f for f in target_file_list if f.endswith("label.pt")]
+            label_pt_path = os.path.join(slice_path, label_pt[0])
+            label_pt_data = torch.load(label_pt_path)
+            label_np_data = label_pt_data.cpu().numpy()
+            norm_label_np = cv2.normalize(
+                label_np_data,
+                None,
+                alpha=0,
+                beta=255,
+                norm_type=cv2.NORM_MINMAX,
             )
-            plt.close()
+            norm_label_np = norm_label_np.astype(np.uint8)
+            label_thres_value, bin_norm_label = cv2.threshold(
+                norm_label_np, 50, 255, cv2.THRESH_BINARY
+            )
+            # print("label_np_data.shape: ", label_np_data.shape)
+            # print("label_np_data.type: ", label_np_data.dtype)
 
-            # DICEやAUROCを計算するため, ラベルデータを読み込む
-            ground_truth_mask = cv2.imread(ground_truth_mask_path, cv2.IMREAD_GRAYSCALE)
-            if ground_truth_mask is None:
-                print(f"ground_truth_mask is None: {ground_truth_mask_path}")
-                sys.exit()
-            else:
-                ground_truth_mask = cv2.normalize(
-                    ground_truth_mask,
-                    None,
-                    alpha=0,
-                    beta=255,
-                    norm_type=cv2.NORM_MINMAX,
-                )
-                _, binarized_ground_truth_mask = cv2.threshold(
-                    ground_truth_mask, 50, 255, cv2.THRESH_BINARY
-                )
-                binarized_ground_truth_mask = binarized_ground_truth_mask.astype(
-                    np.uint8
-                )
-                padded_mask = np.zeros((256, 256), dtype=np.uint8)
-                padded_mask[8:-8, 8:-8] = binarized_ground_truth_mask
+            # # 二値化された入力画像を保存
+            # plt.imshow(bin_norm_diff_all, cmap="gray")
+            # plt.axis("off")
+            # plt.savefig(
+            #     "label_image.jpg",
+            #     bbox_inches="tight",
+            #     pad_inches=0,
+            # )
+            # plt.close()
 
-                plt.imshow(padded_mask, cmap="gray")
-                plt.axis("off")
-                plt.savefig(
-                    f"{diffmap_dirs}/{subject_path[-1]}/{slice_id}/binarized_label_image.jpg",
-                    bbox_inches="tight",
-                    pad_inches=0,
-                )
-                plt.close()
+            # # ptデータのピクセル値のヒストグラムを表示
+            # plt.figure(figsize=(8, 6))
+            # plt.hist(
+            #     bin_norm_diff_all.ravel(),
+            #     bins=100,
+            #     color="b",
+            #     alpha=0.7,
+            #     rwidth=0.8,
+            # )
+            # plt.grid(axis="y", linestyle="--", alpha=0.7)
+            # plt.tight_layout()
+            # plt.ylim(0, 2000)
+            # plt.show()
+            # plt.savefig("label_np_hist.jpg")
+            # plt.close()
+            # sys.exit()
 
-                # 真実のマスクと二値化された画像のDICE係数を計算
-                dice = dice_coefficient(padded_mask, binarized_input_image)
-                dice_list.append(dice)
-                print(f"DICE: {dice}")
+            # ラベル画像と二値化された画像のDICE係数を計算
+            dice = dice_coefficient(bin_norm_label, bin_norm_diff_all)
+            dice_list.append(dice)
+            print(f"DICE: {dice}")
 
-                # AUROCの計算
-                flat_truth = padded_mask.flatten()
-                flat_pred = binarized_input_image.flatten()
+            # AUROCの計算
+            flat_truth = bin_norm_label.flatten()
+            flat_pred = bin_norm_diff_all.flatten()
+            try:
                 auroc = roc_auc_score(flat_truth, flat_pred)
                 auroc_list.append(auroc)
                 print(f"AUROC: {auroc}")
                 print("")
+            except ValueError:
+                print("AUROC cannot be calculated.")
+                print("")
 
-    # DICEとAUROCの値をJSON形式で保存
+    # # DICEとAUROCの値をJSON形式で保存
     dice_and_auroc_json = {}
     dice_avg = np.mean(dice_list)
     auroc_avg = np.mean(auroc_list)
